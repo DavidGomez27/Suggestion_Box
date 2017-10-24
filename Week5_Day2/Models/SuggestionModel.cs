46ea35b09f1f7ced8b26d6ce9d5b35935d8df6fb
DavidGomez27 <mmcsgomez16@gmail.com>
2017-10-24 10:55:10 -0400

    initial commit

---
+++ Week5_Day2/Models/SuggestionModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Week5_Day2.Models
+{
+    public class SuggestionModel
+    {
+
+        public int ID { get; set; }
+
+        public string Topic { get; set; }
+
+        public string Comment { get; set; }
+
+    }
+}
